f83451b8c15e90db5ee7d25799f6bfd82b643faa
Forward-Fax3 <dfordommmmm@gmail.com>
2025-06-03 08:08:20 +0100

    Added 'GLM_FORCE_FMA' to enable FMA instructions with glm

--- premake5.lua
+++ premake5.lua
@@ -141,10 +141,18 @@ project "RTIAW"
 	filter { "platforms:AVX512" }
 --		vectorextensions "AVX512" currently not supported need to use buildoptions instead
 		buildoptions { "/arch:AVX512" }
-		defines { "GLM_FORCE_AVX2" } -- GLM doesnt seem to support AVX512 anymore so we use AVX2 instead though the compiler should still compile it with AVX512
+		defines
+		{
+			"GLM_FORCE_AVX2", -- GLM doesnt seem to support AVX512 anymore so we use AVX2 instead though the compiler should still compile it with AVX512
+			"GLM_FORCE_FMA"
+		} 
 	filter { "platforms:AVX2" }
 		vectorextensions "AVX2"
-		defines { "GLM_FORCE_AVX2" }
+		defines
+		{
+			"GLM_FORCE_AVX2",
+			"GLM_FORCE_FMA"
+		} 
 	filter { "platforms:SSE2" }
 		vectorextensions "SSE2"
 		defines { "GLM_FORCE_SSE2" }
