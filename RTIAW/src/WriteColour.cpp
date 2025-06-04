f52045301a067d36950c53c2c2c68a1005c794e8
Forward-Fax3 <dfordommmmm@gmail.com>
2025-06-04 09:51:22 +0100

    added gamma correction

--- RTIAW/src/WriteColour.cpp
+++ RTIAW/src/WriteColour.cpp
@@ -11,7 +11,10 @@ namespace RTW
 {
 	void WriteColour(std::ostream& out, const Colour& colour)
 	{
-		glm::i16vec3 colourTemp = Interval(0.0, 0.999).Clamp(colour) * 256.0;
+		const Vec3 gamma(2.4);
+		Colour gammaCorrectedColour = glm::pow(colour, 1.0 / gamma);
+
+		glm::i16vec3 colourTemp = Interval(0.0, 0.999).Clamp(gammaCorrectedColour) * 256.0;
 
 		out << colourTemp.r << ' ' << colourTemp.g << ' ' << colourTemp.b << '\n';
 	}
