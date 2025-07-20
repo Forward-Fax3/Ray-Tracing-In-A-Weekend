366d1b22f388b502105980146c5831b61affcd0f
Forward-Fax3 <dfordommmmm@gmail.com>
2025-07-20 19:01:11 +0100

    reluctancly moved to msc as clang doesnt support c++23preview

--- RTIAW/src/Sphere/Sphere.cpp
+++ RTIAW/src/Sphere/Sphere.cpp
@@ -1,17 +1,7 @@
-#include "glm/glm.hpp"
-#include "glm/gtc/random.hpp"
-
 #include "Core.h"
-#include "Interval.h"
 #include "Sphere.h"
-#include "Ray.h"
 
 
 namespace RTW::Templates
 {
-//	template <bool t_IsMoving>
-//	bool Sphere<t_IsMoving>::IsRayHit(const Ray& ray, const Interval& rayDistance, HitData& hitData) const
-
-//	template <bool t_IsMoving>
-//	UV RTW::Templates::Sphere<t_IsMoving>::GetUV(const Point& point)
 }
