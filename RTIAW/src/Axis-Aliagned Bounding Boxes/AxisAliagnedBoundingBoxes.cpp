8257933c7012e00bfb63b659e9c4e80e10505d0c
Forward-Fax3 <dfordommmmm@gmail.com>
2025-07-27 15:56:08 +0100

    done further optimizations but more are still required

--- RTIAW/src/Axis-Aliagned Bounding Boxes/AxisAliagnedBoundingBoxes.cpp
+++ RTIAW/src/Axis-Aliagned Bounding Boxes/AxisAliagnedBoundingBoxes.cpp
@@ -11,6 +11,25 @@
 
 namespace RTW
 {
+	bool CheckOrderedPairs(__m512d vec) {
+		// Shuffle vec so that we get the comparison pairs:
+		// Pair vec[0] with vec[1], vec[2] with vec[3], vec[4] with vec[5]
+		// [0 1 2 3 4 5 6 7]
+		// [1 0 3 2 5 4 x x] (x means we don't care)
+
+		const __m512i m512_SwapBitMap = _mm512_set_epi64(6, 7, 4, 5, 2, 3, 0, 1);
+		__m512d shuffled = _mm512_permutexvar_pd(m512_SwapBitMap, vec);
+
+		// Perform less-than comparisons on relevant pairs
+		__mmask8 mask = _mm512_cmp_pd_mask(vec, shuffled, _CMP_LT_OQ);
+
+		// We only care about bits 0, 2, 4 (corresponding to comparisons 0<1, 2<3, 4<5)
+		// So we mask those out and check if all are set
+		const __mmask8 relevant_mask = 0b00010101; // bits 0, 2, 4
+
+		return (mask & relevant_mask) == relevant_mask;
+	}
+
 	AABB::AxisAliagnedBoundingBoxes(const Interval& interval)
 		: m_X(interval), m_Y(interval), m_Z(interval) { s_NumberofBoundingBoxes++; m_LongestAxis = LongestAxisSetter(); }
 
@@ -46,35 +65,48 @@ namespace RTW
 
 	bool AABB::IsHit(const Ray& ray, Interval rayT) const
 	{
+#if SIMD | AVX512 == 1
 		const __m128d m128_InvertValue = { 1.0, -1.0 };
+#endif
 #if defined(RTW_AVX512) & (AVX512 == 1)
 		const __mmask8 loadMask = 0b00111111;
+//		const __m512i m512_VecLoadMap = _mm512_set_epi64(0, 0, 2, 2, 1, 1, 0, 0);
 		__m512d m512_AxisBounds = _mm512_maskz_load_pd(loadMask, &m_X);
 		const Vec3& rayOrigin = ray.origin();
-		__m512d m512_RayOrigin = { rayOrigin.x, rayOrigin.x, rayOrigin.y, rayOrigin.y, rayOrigin.z, rayOrigin.z, 0.0, 0.0 };
+		__m512d m512_RayOrigin = _mm512_set_pd(0.0, 0.0, rayOrigin.z, rayOrigin.z, rayOrigin.y, rayOrigin.y, rayOrigin.x, rayOrigin.x);
+//		__m512d m512_RayOrigin = _mm512_i64gather_pd(m512_VecLoadMap, &ray.origin(), 8);
+//		__m512d m512_RayOrigin = _mm512_permutexvar_pd(m512_VecLoadMap, ray.origin().data);
 		const Vec3& rayDirection = ray.direction();
-		__m512d m512_RayDirection = { rayDirection.x, rayDirection.x, rayDirection.y, rayDirection.y, rayDirection.z, rayDirection.z, 0.0, 0.0 };
+		__m512d m512_RayDirection = _mm512_set_pd(0.0, 0.0, rayDirection.z, rayDirection.z, rayDirection.y, rayDirection.y, rayDirection.x, rayDirection.x);
+//		__m512d m512_RayDirection = _mm512_i64gather_pd(m512_VecLoadMap, &ray.direction(), 8);
 		__m512d m512_InvRayDiraction = _mm512_div_pd(_mm512_set1_pd(1.0), m512_RayDirection);
 
 		__m512d m512_T = _mm512_mul_pd(_mm512_sub_pd(m512_AxisBounds, m512_RayOrigin), m512_InvRayDiraction);
 
 #if __clang__
-		for (size_t i = 0; i < 6; i += 2)
-			if (m512_T[i] > m512_T[i + 1])
-			{
-				double temp = m512_T[i];
-				m512_T[i] = m512_T[i + 1];
-				m512_T[i + 1] = temp;
-			}
+//		__m256d m512_TMin = { m512_T[0], m512_T[2], m512_T[4], m512_T[6] };
+//		__m256d m512_TMax = { m512_T[1], m512_T[3], m512_T[5], m512_T[7] };
+//
+//		__mmask8 TSwapMask = _mm256_cmp_pd_mask(m512_TMin, m512_TMax, _CMP_GT_OS);
+//		TSwapMask = ScaleBits(TSwapMask, 2);
+//
+//		m512_T = _mm512_mask_permute_pd(m512_T, TSwapMask, m512_T, 0b01010101);
+		
+		const __m512i m512_SwapBitMap = _mm512_set_epi64(6, 7, 4, 5, 2, 3, 0, 1);
+		const __mmask8 m512_SwapBitMask = 0b10101010;
+
+		__m512d m512_T128BitSwaped = _mm512_permutexvar_pd(m512_SwapBitMap, m512_T);
+		__m512d m512_TMin = _mm512_min_pd(m512_T, m512_T128BitSwaped);
+		__m512d m512_TMax = _mm512_max_pd(m512_T, m512_T128BitSwaped);
+
+		m512_T = _mm512_mask_blend_pd(m512_SwapBitMask, m512_TMin, m512_TMax);
+
 #elif _MSC_VER
 		__m256d m512_TMin = { m512_T.m512d_f64[0], m512_T.m512d_f64[2], m512_T.m512d_f64[4], m512_T.m512d_f64[6] };
 		__m256d m512_TMax = { m512_T.m512d_f64[1], m512_T.m512d_f64[3], m512_T.m512d_f64[5], m512_T.m512d_f64[7] };
 
-		__mmask8 TBaseSwapMask = _mm256_cmp_pd_mask(m512_TMin, m512_TMax, _CMP_GT_OS);
-		uint32_t setSwapMaskBitMask = static_cast<uint32_t>(0b01010101);
-		__mmask8 TSwapMask = static_cast<__mmask8>(_pdep_u32(static_cast<uint32_t>(TBaseSwapMask), setSwapMaskBitMask));
-		setSwapMaskBitMask <<= 1;
-		TSwapMask |= static_cast<__mmask8>(_pdep_u32(static_cast<uint32_t>(TBaseSwapMask), setSwapMaskBitMask));
+		__mmask8 TSwapMask = _mm256_cmp_pd_mask(m512_TMin, m512_TMax, _CMP_GT_OS);
+		TSwapMask = ScaleBits(TSwapMask, 2);
 
 		m512_T = _mm512_mask_permute_pd(m512_T, TSwapMask, m512_T, 0b01010101);
 #else
@@ -91,13 +123,19 @@ namespace RTW
 		__m512d m512_MaxMinT = _mm512_mul_pd(_mm512_max_pd(m512_AltInvRayT, m512_AltInvT), m512_InvertValue);
 
 #if __clang__
-		for (size_t i = 0; i < 6; i += 2)
-			if (m512_MaxMinT[i] >= m512_MaxMinT[i + 1])
-				return false;
+//		for (size_t i = 0; i < 6; i += 2)
+//			if (m512_MaxMinT[i] >= m512_MaxMinT[i + 1])
+//				return false;
+
+		return CheckOrderedPairs(m512_MaxMinT);
 #elif _MSC_VER
-		for (size_t i = 0; i < 6; i += 2)
-			if (m512_MaxMinT.m512d_f64[i] >= m512_MaxMinT.m512d_f64[i + 1])
-				return false;
+//		for (size_t i = 0; i < 6; i += 2)
+//			if (m512_MaxMinT.m512d_f64[i] >= m512_MaxMinT.m512d_f64[i + 1])
+//				return false;
+
+		return CheckOrderedPairs(m512_MaxMinT);
+#else
+#error only supports clang and msc
 #endif
 
 #else // RTW_AVX512
@@ -119,23 +157,21 @@ namespace RTW
 			__mmask8 bitMask = _mm_cmp_pd_mask(m128_T, m128_RayT, _CMP_GT_OS);
 			m128_RayT = _mm_mask_load_pd(rayT.GetAsVector().data, bitMask, &t);
 			_mm_store_pd(const_cast<double*>(&rayT.GetAsVector().x), m128_RayT);
+
+#else // RTW_AVX512
+			__m128d bitMask = _mm_cmpgt_pd(m128_T, m128_RayT);
+			m128_RayT = _mm_blendv_pd(rayT.GetAsVector().data, t.data, bitMask);
+			_mm_store_pd(const_cast<double*>(&rayT.GetAsVector().x), m128_RayT);
+#endif // RTW_AVX512
 #if __clang__
 			if (m128_RayT[0] >= m128_RayT[1])
 				return false;
 #elif _MSC_VER
 			if (m128_RayT.m128d_f64[0] >= m128_RayT.m128d_f64[1])
 				return false;
+#else
+#error only supports clang and msc
 #endif
-
-#else // RTW_AVX512
-			__m128d bitMask = _mm_cmpgt_pd(m128_T, m128_RayT);
-			m128_RayT = _mm_blendv_pd(rayT.GetAsVector().data, t.data, bitMask);
-			_mm_store_pd(const_cast<double*>(&rayT.GetAsVector().x), m128_RayT);
-
-			if (m128_RayT.m128d_f64[0] >= m128_RayT.m128d_f64[1])
-				return false;
-
-#endif // RTW_AVX512
 #else // defined(RTW_AVX2) | defined(RTW_AVX512) & (SIMD == 1)
 			if (t.x > rayT.GetMin()) 
 				rayT.SetMin(t.x);
@@ -146,8 +182,8 @@ namespace RTW
 				return false;
 #endif // defined(RTW_AVX2) | defined(RTW_AVX512) & (SIMD == 1)
 		}
-#endif // RTW_AVX512
 		return true;
+#endif // RTW_AVX512
 	}
 
 	RTW::AABB::Axis AxisAliagnedBoundingBoxes::LongestAxisSetter() const
--- RTIAW/src/Core/Core.h
+++ RTIAW/src/Core/Core.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 #include <limits>
 
 #include "glm/glm.hpp"
@@ -21,4 +22,43 @@ namespace RTW
 	Vec3 RandomUnitVector();
 
 	Vec3 RandomOnUnitDisk();
+
+//	template <typename Tout, uint64_t t_ScaleIn, typename Tin>
+//	std::enable_if_t<std::numeric_limits<Tin>::is_signed && std::numeric_limits<Tout>::is_signed, Tout> ScaleBits<Tout, t_ScaleIn>(Tin in)
+//	{
+//		unsigned mask = 0x0FF;
+//		Tout out = static_cast<Tout>(in);
+//		constexpr uint8_t N = sizeof(Tout) * 2;
+//		for (uint64_t scale = t_ScaleIn; scale != 0; scale /= 2)
+//		{
+//			Tout shift = scale * (N - 1);
+//			mask &= ~(mask << scale);
+//			mask |= mask << (scale * N);
+//			out = (out | out << shift) & mask;
+//		}
+//		out *= (1 << N) - 1;
+//		return out;
+//	}
+
+	inline __mmask8 ScaleBits(__mmask8 in, size_t scale)
+	{
+		unsigned mask = 0x0FF;
+		__mmask8 out = in;
+		constexpr uint8_t N = sizeof(__mmask8) * 2;
+		for (; scale != 0; scale /= 2)
+		{
+			__mmask8 shift = scale * (N - 1);
+			mask &= ~(mask << scale);
+			mask |= mask << (scale * N);
+			out = (out | out << shift) & mask;
+		}
+		out *= (1 << N) - 1;
+		return out;
+	}
+
+//	template <typename T, uint64_t t_Scale>
+//	T ScaleBits<t_Scale>(T in)
+//	{
+//		return ScaleBits<T, t_Scale>(in;)
+//	}
 }
