a98a9ff9be660b818868a04b39075711d22330f6
CatGod <dfordommmm+smartgit@gmail.com>
2025-08-03 19:50:02 +0100

    added the ability to colour the perlin noise texture with another texture

--- RTIAW/src/Texture/NoiseTexture/PerlinNoiseTexture/PerlinNoiseTexture.cpp
+++ RTIAW/src/Texture/NoiseTexture/PerlinNoiseTexture/PerlinNoiseTexture.cpp
@@ -1,6 +1,7 @@
 #include "PerlinNoiseTexture.h"
 
 #include "PerlinNoise.h"
+#include "SolidColour.h"
 
 
 namespace RTW
@@ -8,12 +9,18 @@ namespace RTW
 	PerlinNoiseTexture::PerlinNoiseTexture(double scale /*= 1.0*/)
 		: m_Scale(scale)
 	{
+		m_BaseTexture = std::make_shared<SolidColour>(Colour(1.0));
+		m_Noise = std::make_shared<PerlinNoise>();
+	}
+
+	PerlinNoiseTexture::PerlinNoiseTexture(std::shared_ptr<BaseTexture> baseTexture, double scale /*= 1.0*/)
+		: m_Scale(scale), m_BaseTexture(baseTexture)
+	{
 		m_Noise = std::make_shared<PerlinNoise>();
 	}
 
 	Colour PerlinNoiseTexture::GetColour(const UV& uv, const Point& point) const
 	{
-		(void)uv;
-		return Colour(1.0) * m_Noise->Noise(m_Scale * point);
+		return m_BaseTexture->GetColour(uv, point) * m_Noise->Noise(m_Scale * point);
 	}
 }
--- RTIAW/src/Texture/NoiseTexture/PerlinNoiseTexture/PerlinNoiseTexture.h
+++ RTIAW/src/Texture/NoiseTexture/PerlinNoiseTexture/PerlinNoiseTexture.h
@@ -8,10 +8,12 @@ namespace RTW
     {
     public:
         PerlinNoiseTexture(double scale = 1.0);
+        PerlinNoiseTexture(std::shared_ptr<BaseTexture> baseTexture, double scale = 1.0);
 
         virtual Colour GetColour(const UV& uv, const Point& point) const override;
 
     private:
         double m_Scale;
+        std::shared_ptr<BaseTexture> m_BaseTexture;
     };
 }
