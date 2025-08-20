41782c2da7f4d6a13ee1b1f32a9b592c36d3cbf8
CatGod <dfordommmm+smartgit@gmail.com>
2025-08-20 20:45:40 +0100

    cleaned up SolidColour::GetColour

--- RTIAW/src/Texture/SolidColour/SolidColour.h
+++ RTIAW/src/Texture/SolidColour/SolidColour.h
@@ -10,7 +10,7 @@ namespace RTW
     public:
         explicit SolidColour(const Colour& colour);
 
-        Colour GetColour([[maybe_unused]] const UV& uv, [[maybe_unused]] const Point& point) const override { return m_Albedo; }
+        Colour GetColour(const UV&, const Point&) const override { return m_Albedo; }
 
     private:
         Colour m_Albedo;
